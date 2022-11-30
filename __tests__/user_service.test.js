398c74298dc37122179b6e484debde1d47497b72
Andrea Di Maio <andrea.dimaio@it.ibm.com>
2022-11-30 21:37:07 +0100

    Commit exercise

--- __tests__/user_service.test.js
+++ __tests__/user_service.test.js
@@ -2,25 +2,17 @@
 const app = require("../src/server");
 const request = require('supertest')(app);
 
-it("2 + 2 is?", () => {
-  expect(2 + 2).toEqual(4);
+test("2 + 2 is?", () => {
 });
 
-it("Test the Rest API at path /", async () => {
+test("Test the Rest API at path /", async () => {
 
-  const response = await request.get('/')
-  expect(response.statusCode).toEqual(200);
-  expect(response.text).toEqual("Hello from HandsOnLab!");
 });
 
-it("Test the Rest API at path /user", async () => {
+test("Test the Rest API at path /users", async () => {
 
-  const response = await request.get('/users');
-  expect(response.body).toContainEqual({"domain": "", "email": "andrea.dimaio@it.ibm.com", "firstname": "Andrea", "lastname": "Di Maio"});
 });
 
-it("Test the Rest API at path /user/andrea.dimaio@it.ibm.com", async () => {
+test("Test the Rest API at path /user/andrea.dimaio@it.ibm.com", async () => {
 
-  const response = await request.get('/user/andrea.dimaio@it.ibm.com');
-  expect(response.body).toEqual({"domain": "", "email": "andrea.dimaio@it.ibm.com", "firstname": "Andrea", "lastname": "Di Maio"});
 });
