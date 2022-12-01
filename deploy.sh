60eae0d03f9e8da4adb33b4821bb7e55ae01af11
Andrea Di Maio <andrea.dimaio@it.ibm.com>
2022-12-01 13:15:26 +0100

    Update code

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 npm install &> /dev/null
-rm -rf build
-echo -ne "Typescript Build..\r" 
-npm run build &> /dev/null
-echo -e "Typescript Build.. OK"
 echo -ne "Uploading file..\r" 
-scp -r build __scripts__ package.json ibmuser@sandbox-wazi.techgym:/web/nodejs_example_db2
+scp -r build __scripts__ package.json src ibmuser@sandbox-wazi.techgym:/web/nodejs_example_db2
 echo -e "Uploading files.. OK"
